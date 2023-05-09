17e9db8a5693278dde8697c6a5bb13bde3238e1a
nguyendinhthoi <dinhthoi2411@gmail.com>
2023-05-09 16:54:28 +0700

    pratice

---
+++ ss7_variable_datatype_operator/pratice/operator.js
@@ -0,0 +1,6 @@
+height = prompt("Enter the height");
+weight = prompt("Enter the weight");
+let height1 = parseInt(height)
+let weight1 = parseInt(weight)
+let area    = weight1 * height1
+document.write("Area = " + area )
---
+++ ss7_variable_datatype_operator/pratice/variable.js
@@ -0,0 +1,10 @@
+let i = 10
+let f = 20.5
+let b = true
+let s = "ha noi"
+
+
+document.write('i = ' i)
+document.write('f = ' f)
+document.write('b = ' b)
+document.write('s = ' s)
