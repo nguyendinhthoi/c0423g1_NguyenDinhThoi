b3cef215ebb77eb26496ab9beaceee19b53b7957
nguyendinhthoi <dinhthoi2411@gmail.com>
2023-05-17 15:25:07 +0700

    fix homework

--- ss11_loop2/exercise/Show_first_primes/Show_first_primes.js
+++ ss11_loop2/exercise/Show_first_primes/Show_first_primes.js
@@ -1,21 +1,37 @@
-// Kiểm tra được số nguyên tố và in ra số đó
-// Lặp lại đủ 20 lần.
-// đếm số nguyên tố đã tìm được.
-let countPrimeFound = 0;
-let number = 2;
+// // Kiểm tra được số nguyên tố và in ra số đó
+// // Lặp lại đủ 20 lần.
+// // đếm số nguyên tố đã tìm được.
+// let countPrimeFound = 0;
+// let number = 2;
+// do {
+//     // số tự nhiên
+//     let count = 0; // biến này để đếm ước của number.
+//     for (let i = 1; i <= number ; i++) {
+//         if(number % i === 0){
+//             count++;
+//         }
+//     }
+//     if(count === 2) { // nếu số ước bằng 2 thì number là số nguyên tố.
+//         document.write(number + " là số nguyên tố. <br>" );
+//         countPrimeFound++;
+//     }
+//     number++;
+// } while (countPrimeFound < 20);
+//
+//
+let countUoc = 0;
+let number = 1;
 do {
-    // số tự nhiên
-    let count = 0; // biến này để đếm ước của number.
-    for (let i = 1; i <= number ; i++) {
-        if(number % i === 0){
+    let count = 0;
+    for (let i = 1; i <= number; i++) {
+        if (number % i === 0) {
             count++;
         }
     }
-    if(count === 2) { // nếu số ước bằng 2 thì number là số nguyên tố.
-        document.write(number + " là số nguyên tố. <br>" );
-        countPrimeFound++;
+    if (count === 2) {
+        ;
+        document.write(number + `<br>`);
+        countUoc++;
     }
     number++;
-} while (countPrimeFound < 20);
-
-
+} while (countUoc < 20);
