0dc32b1225925437975761cd79de795e5604806d
nguyendinhthoi <dinhthoi2411@gmail.com>
2023-05-18 11:31:04 +0700

    fixed exercise

--- ss12_array/excercise/array_1/bai1/bai1.js
+++ ss12_array/excercise/array_1/bai1/bai1.js
@@ -1,6 +1,6 @@
-num= [1,7,10,13,15,11,12,5,25,17];
-for (i=0 ; i<num.length;i++){
-    if (num[i] >=10){
-        document.writeln(num[i])
+let num = [1, 7, 10, 13, 15, 11, 12, 5, 25, 17];
+for (i = 0; i < num.length; i++) {
+    if (num[i] >= 10) {
+        document.writeln(num[i]);
     }
 }
